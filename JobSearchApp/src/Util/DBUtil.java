70bd257987ce910253955ff7ae05a73484d4e093
Hoang Van Duong <hd2005555@gmail.com>
2025-05-17 23:00:56 +0700

    add dbutil and test connection

---
+++ JobSearchApp/src/Util/DBUtil.java
@@ -0,0 +1,56 @@
+package Util;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	
+	public static Connection MakeConnection ()
+	{
+		Connection conn = null;
+		try {
+			String url = "jdbc:mysql://localhost:3306/findjob";
+			String username = "root";
+			String password = "duong2k5555";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(url,username,password);
+			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	public static void CloseConnection(Connection conn)
+	{
+		try {
+		if(conn != null)
+		{
+			conn.close();
+		}
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+		System.out.println("Test connection : ");
+		Connection conn = MakeConnection();
+		DatabaseMetaData test= (DatabaseMetaData) conn.getMetaData();
+		System.out.print(" ..." +test.getDriverName());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ JobSearchApp/src/module-info.java
@@ -0,0 +1,7 @@
+module JobSearchApp {
+	exports UI;
+	exports Util;
+
+	requires java.desktop;
+	requires java.sql;
+}
